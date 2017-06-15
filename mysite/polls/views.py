95be1c4cb03267dfd08cc5ef48da3eee8907b13c
jahmul144 <erikhummel14@gmail.com>
2017-06-15 19:53:55 +0000

    Another try

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,7 +5,7 @@ from django.core.urlresolvers import reverse
 from mysite.polls.models import Stores
 from mysite.polls.forms import StoreForm
 def index(request):
-	downtown_store = Stores.objects.get(name="E2")
+	downtown_store = Stores.objects.get(name="Corporate")
 	store_name = downtown_store.name
 	store_address = downtown_store.address
 	store_state = downtown_store.state
@@ -17,8 +17,8 @@ def index(request):
 	else:
 		form = StoreForm() 
 	
-	context = {'store_name':store_name, 'store_address':store_address, 'store_state':store_state, 'form':form,} 
+		context = {'store_name':store_name, 'store_address':store_address, 'store_state':store_state, 'form':form,} 
 	    	
-	return render(request,'polls/index.html',context)
+		return render(request,'polls/index.html',context)
 
 
