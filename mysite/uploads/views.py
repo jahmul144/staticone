171d4de4d0694fcaa95ae0d2ad8b68e29fc5313b
jahmul144 <erikhummel14@gmail.com>
2017-07-11 13:41:06 +0000

    Indentation Error

--- mysite/uploads/views.py
+++ mysite/uploads/views.py
@@ -7,11 +7,11 @@ from mysite.uploads.forms import DocumentForm
 # Create your views here. 
 def model_upload(request):
 	if request.method == 'POST':
-	form = DocumentForm(request.POST, request.FILES)
-	if form.is_valid():
-		form.save(commit=True)
-		return HttpResponseRedirect(reverse('uploadindex'))
+		form = DocumentForm(request.POST, request.FILES)
+		if form.is_valid():
+			form.save(commit=True)
+			return HttpResponseRedirect(reverse('uploadindex'))
 	else:
-		print form.errors
+		print (form.errors)
         	#form = DocumentForm()
 	return render(request, 'uploads/index.html', {'form': form})
