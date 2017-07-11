87b65a1de0f28309a9009a4c949f5832f497344b
jahmul144 <erikhummel14@gmail.com>
2017-07-11 13:34:41 +0000

    Spacing error

--- mysite/uploads/views.py
+++ mysite/uploads/views.py
@@ -6,14 +6,12 @@ from mysite.uploads.models import Document
 from mysite.uploads.forms import DocumentForm
 # Create your views here. 
 def model_upload(request):
-    if request.method == 'POST':
-        form = DocumentForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save(commit=True)
-            return HttpResponseRedirect(reverse('uploadindex'))
-    else:
-	print(form.errors)
-        #form = DocumentForm()
-    return render(request, 'uploads/index.html', {
-        'form': form
-    })
+	if request.method == 'POST':
+	form = DocumentForm(request.POST, request.FILES)
+	if form.is_valid():
+		form.save(commit=True)
+		return HttpResponseRedirect(reverse('uploadindex'))
+	else:
+		print(form.errors)
+        	#form = DocumentForm()
+	return render(request, 'uploads/index.html', {'form': form})
