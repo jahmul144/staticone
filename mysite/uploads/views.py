58a270da64f79f222bd9d9ac36cc074f9890f94a
jahmul144 <erikhummel14@gmail.com>
2017-07-11 13:36:01 +0000

    error fixed

--- mysite/uploads/views.py
+++ mysite/uploads/views.py
@@ -12,6 +12,6 @@ def model_upload(request):
 		form.save(commit=True)
 		return HttpResponseRedirect(reverse('uploadindex'))
 	else:
-		print(form.errors)
+		print form.errors
         	#form = DocumentForm()
 	return render(request, 'uploads/index.html', {'form': form})
