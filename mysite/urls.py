b3d8002765e2eb31c5cc138fdd0c4586eb68a71d
jahmul144 <erikhummel14@gmail.com>
2017-07-12 16:13:28 +0000

    added +=

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
 	url(r'^polls/', include('mysite.polls.urls')),
 	url(r'^admin/', admin.site.urls),
 	url(r'^submit/', include('mysite.uploads.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
