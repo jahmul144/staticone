5adfb5f8109501b84f2d4f899feda29f8d239059
jahmul144 <erikhummel14@gmail.com>
2017-07-12 16:53:23 +0000

    Added import serve

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+
  
 urlpatterns = [
 	url(r'^polls/', include('mysite.polls.urls')),
