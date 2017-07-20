a7f70d28bf366ed3f813aeba293222571781aa4b
jahmul144 <erikhummel14@gmail.com>
2017-07-20 19:55:14 +0000

    Updated about page rango

--- mysite/rango/urls.py
+++ mysite/rango/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from mysite.rango  import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='indexrango'),
+	url(r'^$', views.index, name='indexrango'),
+	url(r'^about/', views.about, name='indexrangoabout')
 ]
