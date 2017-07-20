389ed1fef36509f2901e31ce6ffb98ae1d4a38c1
jahmul144 <erikhummel14@gmail.com>
2017-07-20 19:57:53 +0000

    update

--- mysite/rango/views.py
+++ mysite/rango/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 # Create your views here.i
 
 def index(request):
-	return HttpResponse("Rango says hey there partner!"<a href='/rango/about/'>About</a> )
+	return HttpResponse("Rango says hey there partner!" <a href='/rango/about/'>About</a>)
 
 def about(request):
 	return HttpResponse("Rango says here is the about page."<a href="/rango/">Index</a>)
