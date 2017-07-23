32e5125a5787430b1e799a1ffa84a816d949bd6a
jahmul144 <erikhummel14@gmail.com>
2017-07-23 16:18:12 +0000

    Added Models for Rango

--- mysite/rango/models.py
+++ mysite/rango/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Page(models.Model):
+        category = models.ForeignKey('rango.Category')
+        title = models.CharField(max_length=128)
+        views = models.IntegerField(default=0)
+
+        def __str__(self):
+                return self.title
+
+# Create your models here.
