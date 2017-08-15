ffd38b385f20cee8c9f71c44556dc818f174c63e
jahmul144 <erikhummel14@gmail.com>
2017-08-15 17:54:20 +0000

    Update model file to include category model

--- mysite/rango/models.py
+++ mysite/rango/models.py
@@ -3,11 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Page(models.Model):
-        category = models.ForeignKey('rango.Category')
+        category = models.ForeignKey(Category)
         title = models.CharField(max_length=128)
         views = models.IntegerField(default=0)
 
         def __str__(self):
                 return self.title
 
+
 # Create your models here.
+
+class Category(models.Model):
+	name = models.CharField(max_length=128,unique=True)
+
