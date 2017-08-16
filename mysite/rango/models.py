f37ec208dc548b2e07f9a144ef9cd48e24ab5676
jahmul144 <erikhummel14@gmail.com>
2017-08-16 13:23:04 +0000

    error Metal needs to be Meta

--- mysite/rango/models.py
+++ mysite/rango/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Category(models.Model):
 	name = models.CharField(max_length=126,unique=True)
 	
-	class MetaL
+	class Meta
 		verbose_name_plural = "Categories"
 
 class Page(models.Model):
