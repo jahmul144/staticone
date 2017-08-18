cd13063d14344e4161027dd5d1721eeb7e948162
jahmul144 <erikhummel14@gmail.com>
2017-08-18 16:47:39 +0000

    Populate database with script

--- mysite/populate_rango.py
+++ mysite/populate_rango.py
@@ -8,13 +8,65 @@ django.setup()
 from mysite.rango.models import Category,Page
 
 def populate():
-	python_pages = [{"title": "Official Python Tutorial", "url":"http://docs.python.org/2/tutorial/"},{"title":"How to Think like a Computer Scientist",
-			"url":"http://www.greenteapress.com/thinkpython/"}]
 
-	cats = {"Python": {"pages": python_pages}}
+	python_pages = [{"title": "Official Python Tutorial", "url":"http://docs.python.org/2/tutorial/"},{"title":"How to Think like a Computer Scientist",
+			"url":"http://www.greenteapress.com/thinkpython/"},{"title":"Learn Python in 10 Minutes","url":"http://www.korokithakis.net/tutorials/python/"}]
+
+	django_pages = [
+        {"title":"Official Django Tutorial",
+         "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+        {"title":"Django Rocks",
+         "url":"http://www.djangorocks.com/"},
+        {"title":"How to Tango with Django",
+         "url":"http://www.tangowithdjango.com/"} ]	
+
+
+	other_pages = [
+        {"title":"Bottle",
+         "url":"http://bottlepy.org/docs/dev/"},
+        {"title":"Flask",
+         "url":"http://flask.pocoo.org"} ]
+
+
+
+	cats = {"Python": {"pages": python_pages},
+            "Django": {"pages": django_pages},
+            "Other Frameworks": {"pages": other_pages} } 
+
 
 
 	for cat, cat_data in cats.items():
-		print(cats.items())
+		c = add_cat(cat)
+		for p in cat_data["pages"]:
+			add_page(c, p["title"], p["url"])
 
-populate()
+
+	for c in Category.objects.all():
+		for p in Page.objects.filter(category=c):
+			print("- {0} - {1}".format(str(c), str(p)))
+
+def add_page(cat, title, url, views=0):
+    p = Page.objects.get_or_create(category=cat, title=title)[0]
+    p.url=url
+    p.views=views
+    p.save()
+    return p
+
+def add_cat(name):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.save()
+    return c
+
+
+
+	
+	#dica= {"Dog": "1", "ted":"2"}
+
+	#for d, b in dica.items():
+		#print(dica.items())
+		#print(d)
+		#print(b)
+if __name__=='__main__':
+	print("Starting Rango Population Script....")
+	populate()
+
