438baf66d8f4fb489bcd9f1c65e69b604dcfaf9e
jahmul144 <erikhummel14@gmail.com>
2017-08-19 21:06:02 +0000

    adjusted the populate file with likes and views

--- mysite/populate_rango.py
+++ mysite/populate_rango.py
@@ -29,14 +29,15 @@ def populate():
 
 
 
-	cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages} } 
+	cats = {"Python": {"pages": python_pages,"views":128,"likes":64},
+            "Django": {"pages": django_pages,"views":64,"likes":32},
+            "Other Frameworks": {"pages": other_pages,"views":32,"likes":16} } 
 
 
 
 	for cat, cat_data in cats.items():
-		c = add_cat(cat)
+		c = add_cat(cat,cat_data["views"],cat_data["likes"])
+		
 		for p in cat_data["pages"]:
 			add_page(c, p["title"], p["url"])
 
@@ -46,16 +47,18 @@ def populate():
 			print("- {0} - {1}".format(str(c), str(p)))
 
 def add_page(cat, title, url, views=0):
-    p = Page.objects.get_or_create(category=cat, title=title)[0]
-    p.url=url
-    p.views=views
-    p.save()
-    return p
+	p = Page.objects.get_or_create(category=cat, title=title)[0]
+	p.url=url
+	p.views=views
+	p.save()
+	return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
-    c.save()
-    return c
+def add_cat(name,views,likes):
+	c = Category.objects.get_or_create(name=name)[0]
+	c.views=views
+	c.likes=likes
+	c.save()
+	return c
 
 
 
--- mysite/rango/models.py
+++ mysite/rango/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 class Category(models.Model):
 	name = models.CharField(max_length=126,unique=True)
-	
+	likes = models.IntegerField(default=0)
+	views = models.IntegerField(default=0)	
 	class Meta:
 		verbose_name_plural = "Categories"
 
