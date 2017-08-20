d7b4c2add21a1eb377644eb65c957dd3ebb0ad5d
jahmul144 <erikhummel14@gmail.com>
2017-08-20 21:03:26 +0000

    Adding custom admin section

--- mysite/rango/admin.py
+++ mysite/rango/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 
 from mysite.rango.models import Category, Page
 
+class PageAdmin(admin.ModelAdmin):
+	list_display = ('title','category','url')
+
 admin.site.register(Category)
 admin.site.register(Page)
+admin.siet.register(PageAdmin)
+
+
 
 # Register your models here.
