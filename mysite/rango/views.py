49c2960170490c9c6a1f345741785b7aad2210cc
jahmul144 <erikhummel14@gmail.com>
2017-08-22 16:40:00 +0000

    Added slugify

--- mysite/rango/views.py
+++ mysite/rango/views.py
@@ -2,8 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 # Create your views here.i
 
+from mysite.rango.models import Category
+
 def index(request):
-	context_dict = {'boldmessage': "crunchy cookie!"}
+	category_list = Category.objects.order_by('-likes')[:5]
+	context_dict = {'boldmessage': "crunchy cookie!",'categories':category_list}
 	#rango/index is set by the template dir so Django finds finds it through the settings file"
 	return render(request, 'rango/index.html',context=context_dict)
 
