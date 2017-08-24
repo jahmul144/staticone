47ec005c10789d28d51d777e02af60e91a2e0e21
jahmul144 <erikhummel14@gmail.com>
2017-08-24 13:44:19 +0000

    Added CategoryAdmin to admin settings

--- mysite/rango/admin.py
+++ mysite/rango/admin.py
@@ -5,7 +5,10 @@ from mysite.rango.models import Category, Page
 class PageAdmin(admin.ModelAdmin):
 	list_display = ('title','category','url')
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category,CategoryAdmin)
 
 #Custom admin page admin.ModelAdmin is a class representation of a model in the admin interface. Must register model with ModelAdmin object.
 #if you want the standard output then no need for a ModelAdmin object
