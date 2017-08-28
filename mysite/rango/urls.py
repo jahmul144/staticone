be66a3c3d97223c2bf74daaa989fd4a5f931a6fa
jahmul144 <erikhummel14@gmail.com>
2017-08-28 17:41:21 +0000

    Update index template and added slug to url as view parameter

--- mysite/rango/urls.py
+++ mysite/rango/urls.py
@@ -4,5 +4,6 @@ from mysite.rango  import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='indexrango'),
-	url(r'^about/', views.about, name='indexrangoabout')
+	url(r'^about/', views.about, name='indexrangoabout'),
+	url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category,name='show_category'),
 ]
--- mysite/rango/views.py
+++ mysite/rango/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 # Create your views here.i
 
-from mysite.rango.models import Category
+from mysite.rango.models import Category,Page
 
 def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
@@ -14,4 +14,22 @@ def about(request):
 	#Single quote inside double quote to say it is text uftiy use double quote it will fail
 	#html1 = "<html><body>'Rango says this here is the about page!' <a href='/rango/'>Index</a></body></html>"
 	return render(request,'rango/about.html',)
+
+
+def show_category(request,category_name_slug):
+	context_dict = {}
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+		
+		pages = Page.objects.filter(category=category)
+		#Add new item to dictionary with key value pair aDict = {} aDict{'key'] = value pattern
+		context_dict['pages'] = pages
+		context_dict['category'] = category
+
+	except:
+		context_dict['category'] = None
+		context_dict['pages'] = None
+
+
+	return render(request, 'rango/category.html',context=context_dict)
 
