5b71d596d237fa9261f9c980873e7444ca015b77
jahmul144 <erikhummel14@gmail.com>
2017-09-29 14:15:39 +0000

    Can now add pages

--- mysite/rango/forms.py
+++ mysite/rango/forms.py
@@ -17,10 +17,20 @@ class PageForm(forms.ModelForm):
 	title = forms.CharField(max_length=128,help_text="Please enter the title of the page.")
 	url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
 	views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-
+	
 	class Meta:
 		model = Page
 		exclude = ('category',)
 		'''we can either exclude the category field from the form, exclude =('category') or specify the fields to include (i.e. not include
 		the category field) fields = ('title','url', 'views')'''
+	
+	def clean(self):
+		cleaned_data=self.cleaned_data
+		url = cleaned_data.get('url')
+		''' if url is not empty and doesn't start with http:// then prepend http://'''
+		if url and not url.startswith('http://'):
+			url = 'http://' + url
+			cleaned_data['url'] = url
+
+			return cleaned_data
 
