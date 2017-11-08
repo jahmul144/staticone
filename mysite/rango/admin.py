a3aa1bbe91a0a82471bba57d25f3fc7cf911e3ec
jahmul144 <erikhummel14@gmail.com>
2017-11-08 17:56:42 +0000

    Register update

--- mysite/rango/admin.py
+++ mysite/rango/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from mysite.rango.models import Category, Page
+from mysite.rango.models import Category, Page, UserProfile
 
 class PageAdmin(admin.ModelAdmin):
 	list_display = ('title','category','url')
@@ -13,6 +13,8 @@ admin.site.register(Category,CategoryAdmin)
 #Custom admin page admin.ModelAdmin is a class representation of a model in the admin interface. Must register model with ModelAdmin object.
 #if you want the standard output then no need for a ModelAdmin object
 admin.site.register(Page,PageAdmin)
+
+admin.site.register(UserProfile)
 
 
 
--- mysite/rango/models.py
+++ mysite/rango/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.template.defaultfilters import slugify
 # Create your models here.
 
+from django.contrib.auth.models import User
+
 class Category(models.Model):
 	name = models.CharField(max_length=126,unique=True)
 	views = models.IntegerField(default=0)
@@ -25,6 +27,18 @@ class Page(models.Model):
 
 	def __str__(self):
 		return self.title
+
+class UserProfile(models.Model):
+	
+	user = models.OneToOneField(User)
+	
+	website = models.URLField(blank=True)
+	picture = models.ImageField(upload_to='profile_images', blank=True)
+	
+	def __str__(self):
+		return self.user.username
+
+	 
 
 
 # Create your models here.
